4b87d290a97802e3837e39262779eae88b85e536
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-03 15:36:28 +0300

    feat(favorites): implement getAll method in FavoritesService

--- src/favorites/favorites.service.ts
+++ src/favorites/favorites.service.ts
@@ -3,6 +3,10 @@ import { InMemoryFavoritesStore, Favorites } from './entities/favorites.entity';
 import { ArtistService } from '../artist/artist.service';
 import { AlbumService } from '../album/album.service';
 import { TrackService } from '../track/track.service';
+import { Artist } from '../artist/entities/artist.entity';
+import { FavoritesResponseDto } from './dto/favorites-response.dto';
+import { Album } from '../album/entities/album.entity';
+import { Track } from '../track/entities/track.entity';
 
 @Injectable()
 export class FavoritesService {
@@ -16,4 +20,32 @@ export class FavoritesService {
     @Inject(forwardRef(() => TrackService))
     private readonly trackService: TrackService,
   ) {}
+
+  async getAll(): Promise<FavoritesResponseDto> {
+    const artists: Artist[] = [];
+    for (const artistId of this.favorites.artists) {
+      try {
+        const artist = this.artistService.findOne(artistId);
+        artists.push(artist);
+      } catch (error) {}
+    }
+
+    const albums: Album[] = [];
+    for (const albumId of this.favorites.albums) {
+      try {
+        const album = this.albumService.findOne(albumId);
+        albums.push(album);
+      } catch (error) {}
+    }
+
+    const tracks: Track[] = [];
+    for (const trackId of this.favorites.tracks) {
+      try {
+        const track = this.trackService.findOne(trackId);
+        tracks.push(track);
+      } catch (error) {}
+    }
+
+    return { artists, albums, tracks };
+  }
 }
