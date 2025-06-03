5288a70867eefd6bcd9703a5de8f09e95f3919cc
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-03 13:58:57 +0300

    feat(track): implement create method in TrackService

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@nestjs/common';
 import { Track } from './entities/track.entity';
+import { v4 as uuidv4 } from 'uuid';
+import { CreateTrackDto } from './dto/create-track.dto';
 
 @Injectable()
 export class TrackService {
   private tracks: Track[] = [];
+
+  create(createTrackDto: CreateTrackDto): Track {
+    const newTrack: Track = {
+      id: uuidv4(),
+      name: createTrackDto.name,
+      artistId:
+        createTrackDto.artistId === undefined ? null : createTrackDto.artistId,
+      albumId:
+        createTrackDto.albumId === undefined ? null : createTrackDto.albumId,
+      duration: createTrackDto.duration,
+    };
+    this.tracks.push(newTrack);
+    return newTrack;
+  }
 }
