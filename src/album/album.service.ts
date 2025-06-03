53e64435b16fdd4058404b4b14a3a2bcfae8bf94
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-03 13:30:02 +0300

    feat(album): implement create method in AlbumService

--- src/album/album.service.ts
+++ src/album/album.service.ts
@@ -1,7 +1,20 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 import { Album } from './entities/album.entity';
+import { CreateAlbumDto } from './dto/create-album.dto';
 
 @Injectable()
 export class AlbumService {
   private albums: Album[] = [];
+
+  create(createAlbumDto: CreateAlbumDto): Album {
+    const newAlbum: Album = {
+      id: uuidv4(),
+      name: createAlbumDto.name,
+      year: createAlbumDto.year,
+      artistId: createAlbumDto.artistId === undefined ? null : createAlbumDto.artistId,
+    };
+    this.albums.push(newAlbum);
+    return newAlbum;
+  }
 }
