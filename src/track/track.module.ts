b9e9b66cf5993864b9f6de4245df1bfcf65f152b
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-03 15:09:33 +0300

    feat(track): implement track controller with all CRUD endpoints

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { TrackService } from './track.service';
+import { AppController } from '../app.controller';
 
 @Module({
+  controllers: [AppController],
   providers: [TrackService],
   exports: [TrackService],
 })
