341b03b5688bd8d36690a20b0eeaa9e424364a38
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-03 15:06:22 +0300

    feat(track): implement update method in TrackService

--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Track } from './entities/track.entity';
 import { v4 as uuidv4 } from 'uuid';
 import { CreateTrackDto } from './dto/create-track.dto';
+import { UpdateTrackDto } from './dto/update-track.dto';
+
 
 @Injectable()
 export class TrackService {
@@ -31,5 +33,28 @@ export class TrackService {
       throw new NotFoundException(`Track with ID ${id} not found`);
     }
     return track;
+  }
+
+  update(id: string, updateTrackDto: UpdateTrackDto): Track {
+    const trackIndex = this.tracks.findIndex((t) => t.id === id);
+    if (trackIndex === -1) {
+      throw new NotFoundException(`Track with ID ${id} not found`);
+    }
+
+    const existingTrack = this.tracks[trackIndex];
+    const updatedTrack = {
+      ...existingTrack,
+      ...updateTrackDto,
+    };
+
+    if (updateTrackDto.hasOwnProperty('artistId')) {
+      updatedTrack.artistId = updateTrackDto.artistId;
+    }
+    if (updateTrackDto.hasOwnProperty('albumId')) {
+      updatedTrack.albumId = updateTrackDto.albumId;
+    }
+
+    this.tracks[trackIndex] = updatedTrack;
+    return updatedTrack;
   }
 }
