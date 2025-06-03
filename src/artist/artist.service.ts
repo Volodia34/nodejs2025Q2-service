3f1458254382516f8a856de61d55ae972ffb2d7d
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-03 13:11:52 +0300

    feat(artist): implement create method in ArtistService

--- src/artist/artist.service.ts
+++ src/artist/artist.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 import { Artist } from './entities/artist.entity';
+import { CreateArtistDto } from './dto/create-artist.dto';
 
 @Injectable()
 export class ArtistService {
   private artists: Artist[] = [];
+
+  create(createArtistDto: CreateArtistDto): Artist {
+    const newArtist: Artist = {
+      id: uuidv4(),
+      name: createArtistDto.name,
+      grammy: createArtistDto.grammy,
+    };
+    this.artists.push(newArtist);
+    return newArtist;
+  }
 }
