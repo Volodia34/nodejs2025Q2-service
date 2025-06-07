66ababc1721ba3371e81594dcfe546ef68b39a8e
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-07 16:34:35 +0300

    feat(favorites): import favorites entity into module

--- src/favorites/favorites.module.ts
+++ src/favorites/favorites.module.ts
@@ -4,9 +4,12 @@ import { ArtistModule } from '../artist/artist.module';
 import { AlbumModule } from '../album/album.module';
 import { TrackModule } from '../track/track.module';
 import { FavoritesController } from './favorites.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Favorites } from './entities/favorites.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Favorites]),
     forwardRef(() => ArtistModule),
     forwardRef(() => AlbumModule),
     forwardRef(() => TrackModule),
