e7eff44381a32bbc7d5a5e49f750d29110d953c9
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-07 15:57:07 +0300

    feat(album): create album entity

--- src/album/entities/album.entity.ts
+++ src/album/entities/album.entity.ts
@@ -1,6 +1,16 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity()
 export class Album {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
+
+  @Column()
   name: string;
+
+  @Column()
   year: number;
+
+  @Column({ type: 'uuid', nullable: true })
   artistId: string | null;
 }
