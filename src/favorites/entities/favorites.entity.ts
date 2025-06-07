6704fc53a0dd9d22d6a442e4305658bf1549934f
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-07 16:32:52 +0300

    feat(favorites): create favorites entity with relations

--- src/favorites/entities/favorites.entity.ts
+++ src/favorites/entities/favorites.entity.ts
@@ -1,10 +1,33 @@
-export interface Favorites {
-  artists: string[];
-  albums: string[];
-  tracks: string[];
+import { Album } from '../../album/entities/album.entity';
+import { Artist } from '../../artist/entities/artist.entity';
+import { Track } from '../../track/entities/track.entity';
+import {
+  Entity,
+  JoinColumn,
+  ManyToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('favorites')
+export class Favorites {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ManyToMany(() => Artist, { eager: true, onDelete: 'CASCADE' })
+  @JoinColumn()
+  artists: Artist[];
+
+  @ManyToMany(() => Album, { eager: true, onDelete: 'CASCADE' })
+  @JoinColumn()
+  albums: Album[];
+
+  @ManyToMany(() => Track, { eager: true, onDelete: 'CASCADE' })
+  @JoinColumn()
+  tracks: Track[];
 }
 
 export const InMemoryFavoritesStore: Favorites = {
+  id: '',
   artists: [],
   albums: [],
   tracks: [],
