a5776b0df820a9e5f9c072a30a66bc67d453b47e
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-07 16:25:45 +0300

    feat(track): import track entity into module

--- src/track/track.module.ts
+++ src/track/track.module.ts
@@ -2,9 +2,12 @@ import { forwardRef, Module } from '@nestjs/common';
 import { TrackService } from './track.service';
 import { TrackController } from './track.controller';
 import { FavoritesModule } from '../favorites/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Track } from './entities/track.entity';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Track]),
     forwardRef(() => FavoritesModule),
   ],
   controllers: [TrackController],
