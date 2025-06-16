750cc979e95ecb611a7b950cc4eebb10786c2b90
Volodia34 <120255295+Volodia34@users.noreply.github.com>
2025-06-16 23:16:11 +0300

    feat(auth): implement signup and login logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(private authService: AuthService) {}
+
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
+  signup(@Body() createUserDto: CreateUserDto) {
+    return this.authService.signUp(createUserDto);
+  }
+
+  @Post('login')
+  @HttpCode(HttpStatus.OK)
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UserService } from '../user/user.service';
+import { CreateUserDto } from '../user/dto/create-user.dto';
+import { LoginDto } from './dto/login.dto';
+import * as bcrypt from 'bcrypt';
 import { ConfigService } from '@nestjs/config';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
@@ -10,4 +14,44 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly configService: ConfigService,
   ) {}
+
+  async signUp(createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
+
+  async login(loginDto: LoginDto) {
+    const user = await this.userService.findOneByLogin(loginDto.login);
+    if (!user) {
+      throw new UnauthorizedException('Authentication failed');
+    }
+
+    const isPasswordMatch = await bcrypt.compare(
+      loginDto.password,
+      user.password,
+    );
+    if (!isPasswordMatch) {
+      throw new UnauthorizedException('Authentication failed');
+    }
+
+    return this.generateTokens(user);
+  }
+
+  private async generateTokens(user: User) {
+    const payload = { userId: user.id, login: user.login };
+
+    const accessToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_SECRET_KEY'),
+      expiresIn: this.configService.get<string>('TOKEN_EXPIRE_TIME'),
+    });
+
+    const refreshToken = await this.jwtService.signAsync(payload, {
+      secret: this.configService.get<string>('JWT_SECRET_REFRESH_KEY'),
+      expiresIn: this.configService.get<string>('TOKEN_REFRESH_EXPIRE_TIME'),
+    });
+
+    return {
+      accessToken,
+      refreshToken,
+    };
+  }
 }
